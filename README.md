52617ee4128eb1dc6b91e02f7bfe60ebc534ae04
tinyjin <baram991103@gmail.com>
2019-10-21 23:53:31 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Korean Name Generator [![pypi][pypi-image]][pypi-url]
+
+[pypi-image]: https://img.shields.io/pypi/v/korean-name-generator.svg
+[pypi-url]: https://img.shields.io/pypi/v/korean-name-generator
+
+한국어 이름을 랜덤으로 작명해 주는 라이브러리입니다. 남자 이름과 여자 이름 중 하나를 지정하면 그에 어울리는 이름이 생성됩니다.
+
+## 설치하기
+```
+pip install korean-name-generator
+```
+
+## 사용하기
+```python
+from korean_name_generator import generate
+
+# 남자 이름 생성
+male_name = generate(True)
+
+# 여자 이름 생성
+female_name = generate(False)
+```
+
+## 작명 원리
+
+초성, 중성, 종성의 자모별 연관 관계를 [인접행렬](https://en.wikipedia.org/wiki/Adjacency_matrix) 형태로 도출한 후, 아래와 같은 간단한 확률 모델을 사용하여 *어울릴 법한* 자모들을 조합하는 방식입니다. 초성이 선택되면 중성의 확률변수 `A`가 정해지고, 중성이 정해지면 종성의 확률변수 `B*C`가 정해집니다.
+
+![Diagram](https://user-images.githubusercontent.com/6297755/29570112-74a030aa-8790-11e7-906a-e479e982fe08.png)
+
+현재 기본적으로 남자 이름은 약 10000개, 여자 이름은 1000개가 학습되어 있으나, 다른 이름을 학습 데이터로 사용할 수도 있습니다.
+```python
+from korean_name_generator import train, generateCustom
+names = ['홍범도', '안중근', '유관순', '이봉창', '김좌진']
+
+# 학습
+trainedData = train(names)
+
+# 학습된 데이터를 바탕으로 이름 생성
+name = generateCustom(trainedData)
+```
+
+## 라이센스
+이 프로젝트는 [MIT]() 라이센스에 따라 자유롭게 이용 가능합니다.
---
+++ korean_name_generator.py
@@ -0,0 +1,211 @@
+# Converted From https://github.com/agemor/korean-name-generator
+
+"""
+MIT License
+
+Copyright (c) 2017 HyunJun Kim
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import trained_data as trainedData
+import random
+import math
+
+
+def generateCustom(trainedDataMatrix):
+    """
+    랜덤한 한국 이름을 생성한다.
+
+    :arg isMale: 남자 또는 여자의 성별 여부.
+    """
+
+    def ensure(n):
+        return n or 0
+
+    # 요소들의 가중치에 비례한 확률로 랜덤 뽑기
+    def pick(count, item):
+        sum = 0
+        selected = 0
+
+        for i in range(count):
+            sum += item(i)
+
+        pivot = random.random() * sum
+
+        for i in range(count):
+            pivot -= item(i)
+            if pivot <= 0:
+                selected = i
+                break
+
+        return selected
+
+    # 랜덤으로 음절 생성
+    def pickSyllable(set):
+        try:
+            choseong = pick(19, lambda n: ensure(trainedDataMatrix[set][0][n]))
+        except Exception:
+            choseong = 0
+
+        try:
+            jungseong = pick(21, lambda n: ensure(trainedDataMatrix[set][1][choseong * 21 + n]))
+        except Exception:
+            jungseong = 0
+
+        try:
+            jongseong = pick(28, lambda n: ensure(trainedDataMatrix[set][2][jungseong * 28 + n]) * ensure(trainedDataMatrix[set][3][choseong * 28 + n]))
+        except Exception:
+            jongseong = 0
+
+        return constructFromJamoIndex([choseong, jungseong, jongseong])
+
+    def pickLastName():
+        lastNameIndex = pick(len(trainedData.lastNames), lambda n: trainedData.lastNameFrequency[n])
+
+        return chr(trainedData.lastNames[lastNameIndex] + 0xAC00)
+
+    return pickLastName() + pickSyllable(0) + pickSyllable(1)
+
+
+def generate(isMale=True):
+    return generateCustom(trainedData.firstNames[0 if isMale else 1])
+
+
+def train(nameList, compress=False):
+    """
+    이름 리스트를 토대로 통계적 학습 데이터를 생성한다.
+
+    :arg nameList: 학습할 이름 목록
+    """
+
+    trainedNameData = [[[], [], [], []], [[], [], [], []]]
+
+    def increase(array, index):
+        array[index] = (array[index] + 1 if array[index] else 1)
+    
+
+    def process(set, jamo):
+        increase(trainedNameData[set][0], jamo[0])
+        increase(trainedNameData[set][1], jamo[0] * 21 + jamo[1])
+        increase(trainedNameData[set][2], jamo[1] * 28 + jamo[2])
+        increase(trainedNameData[set][3], jamo[0] * 28 + jamo[2])
+
+    for i in range(len(nameList)):
+        firstName = nameList[i].substring(1)
+
+        cheot = firstName.charAt(0)
+        du = firstName.charAt(1) if len(firstName) > 0 else ''
+
+        cheotJamo = resolveToJamoIndex(cheot)
+        duJamo = resolveToJamoIndex(du)
+
+        if cheotJamo: process(0, cheotJamo)
+        if duJamo: process(1, duJamo)
+
+    return compressEmptyPart(trainedNameData) if compress else trainedNameData
+
+
+def compressEmptyPart(array):
+    """
+    빈 공간이 많은 배열을 압축한다.
+
+    :param array: 압축할 대상 배열
+    """
+
+    compressedArray = []
+    emptyCount = 0
+
+    for i in range(len(array)):
+
+        if not array[i]:
+            emptyCount += 1
+        elif type(array[i]) == list:
+            compressedArray.append(compressEmptyPart(array[i]))
+
+        else:
+            if emptyCount > 0:
+                compressedArray.append(-emptyCount)
+                emptyCount = 0
+                
+            compressedArray.append(array[i])
+
+    return compressedArray
+
+
+def uncompressEmptyPart(array):
+    """
+    압축된 배열을 원래 상태로 되돌린다.
+
+    :arg array: 압축 해제할 대상 배열
+    """
+    originalArray = []
+
+    for i in range(len(array)):
+
+        if type(array[i]) == list:
+            originalArray.append(uncompressEmptyPart(array[i]))
+
+        else:
+
+            if array[i] >= 0:
+                originalArray.append(array[i])
+            else:
+                for j in range(-array[i]):
+                    originalArray.append(0)
+
+    return originalArray
+
+
+def constructFromJamoIndex(jamoIndex):
+    """
+    자모 배열로부터 음절을 생성한다.
+
+    :param jamoIndex:
+    """
+    return chr(0xAC00 + 28 * 21 * jamoIndex[0] + 28 * jamoIndex[1] + jamoIndex[2])
+
+
+def resolveToJamoIndex(syllable):
+    """
+    음절로부터 자보 배열을 생성한다.
+
+    :param syllable:
+    """
+
+    code = syllable.charCodeAt(0) - 0xAC00
+
+    choseong = math.floor(((code - code % 28) / 28) / 21)
+    jungseong = math.floor(((code - code % 28) / 28) % 21)
+    jongseong = code % 28
+
+    def isValid(n):
+        return n >= 0 if not n.isdigit() else False
+
+    if not (isValid(choseong) and isValid(jungseong) and isValid(jongseong)):
+        return None
+
+    return [choseong, jungseong, jongseong]
+
+trainedData.firstNames = uncompressEmptyPart(trainedData.firstNames)
+
+
+if __name__ == '__main__':
+    maleName = generate(True)
+    print(maleName)
